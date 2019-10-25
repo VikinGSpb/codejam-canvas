e3d2bf3a7b7e572d14fc2d4aa481028886c12582
VikinGSpb <imbot@inbox.ru>
2019-10-25 13:18:15 +0300

    feat: add 32x32 paint

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let button2 = document.querySelector('#thirtytwox');
 let button3 = document.querySelector('#img');
 let canvas = document.querySelector('#canvas');
 
-function draw(arr) {
+function draw1(arr) {
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
 
@@ -11,6 +11,19 @@ function draw(arr) {
             row.forEach((column, j) => {
                 ctx.fillStyle = "#" + column;
                 ctx.fillRect(j * 128, i * 128, (j + 1) * 128, (i + 1) * 128);
+            })
+        })
+    }
+}
+
+function draw2(arr) {
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        arr.forEach((row, i) => {
+            row.forEach((column, j) => {
+                ctx.fillStyle = "rgba(" + column + ")";
+                ctx.fillRect(j * 16, i * 16, (j + 1) * 16, (i + 1) * 16);
             })
         })
     }
@@ -24,7 +37,20 @@ button1.addEventListener('click', () => {
     let arr = [];
     xhr.onload = () => {
         arr = xhr.response;
-        draw(arr);
+        draw1(arr);
+        alert(arr);
+    }
+})
+
+button2.addEventListener('click', () => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/32x32.json');
+    xhr.responseType = 'json';
+    xhr.send();
+    let arr = [];
+    xhr.onload = () => {
+        arr = xhr.response;
+        draw2(arr);
         alert(arr);
     }
 })
