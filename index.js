d5a56c2551e076e76d38a5877c151b590110c257
VikinGSpb <imbot@inbox.ru>
2019-11-20 00:00:05 +0300

    fix: mentor remarks

--- index.js
+++ index.js
@@ -1,65 +1,52 @@
-let button1 = document.querySelector('#fourx');
-let button2 = document.querySelector('#thirtytwox');
-let button3 = document.querySelector('#img');
-let canvas = document.querySelector('#canvas');
+const button3 = document.querySelector('#img');
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
 
-function draw1(arr) {
-    if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
 
-        arr.forEach((row, i) => {
-            row.forEach((column, j) => {
-                ctx.fillStyle = "#" + column;
-                ctx.fillRect(j * (512 / arr.length), i * (512 / arr.length), (j + 1) * (512 / arr.length), (i + 1) * (512 / arr.length));
+function draw(arr) {
+    if (canvas && canvas.getContext) {
+        arr.forEach((row, rowIdx) => {
+            row.forEach((column, colIdx) => {
+                if(column.length === 6) {
+                    ctx.fillStyle = "#" + column;
+                } else {
+                    ctx.fillStyle = "rgba(" + column + ")";
+                }
+                ctx.fillRect(colIdx * 512 / arr.length, rowIdx * 512 / arr.length, (colIdx + 1) * 512 / arr.length, (rowIdx + 1) * 512 / arr.length);
             })
         })
-    }
+    } else throw new Error('Canvas Error');
 }
 
-function draw2(arr) {
-    if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
 
-        arr.forEach((row, i) => {
-            row.forEach((column, j) => {
-                ctx.fillStyle = "rgba(" + column + ")";
-                ctx.fillRect(j * (512 / arr.length), i * (512 / arr.length), (j + 1) * (512 / arr.length), (i + 1) * (512 / arr.length));
-            })
-        })
-    }
+for(let button of document.querySelectorAll('[canvasButton]')) {
+    button.addEventListener('click', () => {
+        let xhr = new XMLHttpRequest();
+        let type = button.getAttribute('canvasButton');
+        xhr.open('GET', 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/' + type + '.json');
+        xhr.responseType = 'json';
+        xhr.send();
+        xhr.onload = () => {
+            let arr = [];
+            arr = xhr.response;
+            draw(arr);
+        }
+        xhr.onerror = () => {
+            throw new Error('Data Error');
+        }
+    });
 }
 
-button1.addEventListener('click', () => {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json');
-    xhr.responseType = 'json';
-    xhr.send();
-    let arr = [];
-    xhr.onload = () => {
-        arr = xhr.response;
-        draw1(arr);
-    }
-})
-
-button2.addEventListener('click', () => {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/32x32.json');
-    xhr.responseType = 'json';
-    xhr.send();
-    let arr = [];
-    xhr.onload = () => {
-        arr = xhr.response;
-        draw2(arr);
-    }
-})
 
 button3.addEventListener('click', () => {
     let image = document.createElement('img');
     image.setAttribute('src','https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/image.png');
     image.onload = () => {
-        if (canvas.getContext) {
-            var ctx = canvas.getContext('2d');
+        if (canvas && canvas.getContext) {
             ctx.drawImage(image, 0, 0, 512, 512);
-        }
+        } else throw new Error('Canvas Error');
     }
-})
+    image.onerror = () => { 
+        throw new Error('Data Error');
+    }
+});
